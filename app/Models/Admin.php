a2794885ca589de4eccd6d29be2a28be807e8b00
Tuan_Anh_24801 <tuanh12a12001@gmail.com>
2024-01-31 18:21:21 +0700

    change name table

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -11,7 +11,7 @@ class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'Admins';
+    protected $table = 'admins';
 
     protected $fillable = [
         'name',
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -19,7 +19,7 @@ class Customer extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'Customers';
+    protected $table = 'customers';
 
     protected $fillable = [
         'name',
