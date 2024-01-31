2d53e6d8d922013a39ff867def6e1383fe654b5b
Tuan_Anh_24801 <tuanh12a12001@gmail.com>
2024-02-01 00:14:06 +0700

    add id autocomplete

--- resources/views/admin/errand-worker-management/create.blade.php
+++ resources/views/admin/errand-worker-management/create.blade.php
@@ -24,7 +24,7 @@
 
             <div class="mb-3">
                 <label  class="form-label">Địa chỉ</label>
-                <input type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}">
+                <input type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" id="autocomplete">
                 {{-- <input type="text" class="form-control" id="autocomplete"> --}}
                 @error('address') <span class="text-danger">{{ $message }}</span> @enderror
             </div>
@@ -79,6 +79,26 @@
                 reader.readAsDataURL(this.files[0]);
             });
         });
+
+        let autocomplete;
+        function initAutocomplete() {
+            autocomplete = new google.maps.places.Autocomplete(
+                document.getElementById('autocomplete'), {
+                types: ['address'],
+                componentRestrictions: { 'country': ['VN'] },
+                fields: ['name']
+            });
+            autocomplete.addListener('place_changed', onPlaceChanged);
+        }
+
+        function onPlaceChanged() {
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                document.getElementById('autocomplete').placeholder = 'Enter a location';
+            } else {
+                document.getElementById('autocomplete').innerHTML = place.name;
+            }
+        }
 
     </script>
 @endsection
